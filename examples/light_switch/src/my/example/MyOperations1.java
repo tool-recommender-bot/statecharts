59c942d99e120f753359f61ce4479b50f1e2dd83
Rainer Klute <rainer.klute@gmx.de>
2016-05-31 11:18:11 +0200

    Light switch extended: intermediate result

---
+++ examples/light_switch/src/my/example/MyOperations1.java
@@ -0,0 +1,9 @@
+package my.example;
+
+public class MyOperations1 {
+
+	public long getTimeOfDay() {
+		return 42;
+	}
+
+}
---
+++ examples/light_switch/src/my/example/MyOperations2.java
@@ -0,0 +1,9 @@
+package my.example;
+
+public class MyOperations2 {
+
+	public double random() {
+		return Math.random();
+	}
+
+}
