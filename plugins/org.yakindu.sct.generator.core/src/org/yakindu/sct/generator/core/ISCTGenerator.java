54f38084bdaca5b3eb231e935881e97b7e18e064
Andreas Muelder <muelder@itemis.de>
2016-08-15 18:33:08 +0200

    Generator executor

--- plugins/org.yakindu.sct.generator.core/src/org/yakindu/sct/generator/core/ISCTGenerator.java
+++ plugins/org.yakindu.sct.generator.core/src/org/yakindu/sct/generator/core/ISCTGenerator.java
@@ -1,29 +1,22 @@
-/**
- * Copyright (c) 2011 committers of YAKINDU and others.
- * All rights reserved. This program and the accompanying materials
- * are made available under the terms of the Eclipse Public License v1.0
- * which accompanies this distribution, and is available at
- * http://www.eclipse.org/legal/epl-v10.html
- *
+/** 
+ * Copyright (c) 2015 committers of YAKINDU and others. 
+ * All rights reserved. This program and the accompanying materials 
+ * are made available under the terms of the Eclipse Public License v1.0 
+ * which accompanies this distribution, and is available at 
+ * http://www.eclipse.org/legal/epl-v10.html 
  * Contributors:
- *     committers of YAKINDU - initial API and implementation
- */
+ * committers of YAKINDU - initial API and implementation
+ *
+*/
 package org.yakindu.sct.generator.core;
 
-import org.yakindu.sct.model.sgen.GeneratorEntry;
-import org.yakindu.sct.model.sgen.GeneratorModel;
-
 /**
- * Basic interface for all CodeGenerators
+ * Marker interface for all code generators, implementations should extend from
+ * {@link IExecutionFlowGenerator} or ISGraphGenerator
  * 
  * @author andreas muelder - Initial contribution and API
  * 
  */
 public interface ISCTGenerator {
-	/**
-	 * 
-	 * @param entry
-	 *            The {@link GeneratorEntry} from the {@link GeneratorModel}
-	 */
-	public void generate(GeneratorEntry entry);
+
 }
