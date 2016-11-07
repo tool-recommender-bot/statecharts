256993275cc8e3c4366ac6de39e7f3085ce126a1
Rene Beckmann <rene.bckmnn@gmail.com>
2016-11-07 11:09:50 +0100

    Add functionality to read back test result

--- test-plugins/org.yakindu.sct.generator.python3.test/src/org/yakindu/sct/generator/python3/pyunittest/PythonTestRunner.java
+++ test-plugins/org.yakindu.sct.generator.python3.test/src/org/yakindu/sct/generator/python3/pyunittest/PythonTestRunner.java
@@ -20,16 +20,31 @@ import java.util.regex.Pattern;
 
 import org.junit.runner.Description;
 import org.junit.runner.Runner;
+import org.junit.runner.notification.Failure;
 import org.junit.runner.notification.RunNotifier;
 import org.junit.runners.model.InitializationError;
+
+import junit.framework.AssertionFailedError;
 
 /**
  * @author rbeckmann
  *
  */
 public class PythonTestRunner extends Runner {
+	/*
+	 * Regex Patterns
+	 */
 	private static final Pattern TESTNAME_PATTERN = Pattern.compile(
 			"([\\D]+[\\w]*) \\(([\\D]+[\\w]*)[.]([\\D]+[\\w]*)\\s*\\)");
+	private static final Pattern TRACEBACK_PATTERN = Pattern.compile(
+			"(?s)-*\\s(Traceback.*)\\s*-{70}.*-*");
+	private static final Pattern TEST_FAIL_PATTERN = Pattern.compile(
+			"FAIL");
+	private static final Pattern TEST_ERROR_PATTERN = Pattern.compile(
+			"ERROR");
+	/*
+	 * 
+	 */
 	private Class<?> testClass;
 	private PyTest annotation;
 	private ArrayList<String> tests;
@@ -86,7 +101,8 @@ public class PythonTestRunner extends Runner {
 		String pythonModule = annotation.path().replace('/', '.');
 		
 		for(String test: tests) {
-			notifier.fireTestStarted(Description.createTestDescription(testClass, test));
+			Description tcDescription = Description.createTestDescription(testClass, test);
+			notifier.fireTestStarted(tcDescription);
 			String testpath = pythonModule + "." + test;
 			Process process = new ProcessBuilder("python3", "-m", "unittest", testpath).redirectErrorStream(true).directory(null).start();
 			BufferedReader reader = new BufferedReader(new InputStreamReader(process.getInputStream()));
@@ -95,14 +111,36 @@ public class PythonTestRunner extends Runner {
 			while((line = reader.readLine()) != null) {
 				b.append(line + "\n");
 			}
-			System.out.print(b.toString());
-			notifier.fireTestFinished(Description.createTestDescription(testClass, test));
+			
+			// Handle test output
+			TestOutput out = parseTestOutput(b.toString());
+			
+			if(!out.getPass()) {
+				Failure failure = new Failure(tcDescription, new AssertionFailedError(out.getTraceback().toString()));
+				notifier.fireTestFailure(failure);
+			}
+			notifier.fireTestFinished(tcDescription);
+			
 		}
 		
 	}
 	
-	private void parseTestOutput(String s) {
+	private TestOutput parseTestOutput(String s) {
+		Matcher matcher_fail = TEST_FAIL_PATTERN.matcher(s);
+		Matcher matcher_error = TEST_ERROR_PATTERN.matcher(s);
+		int status = TestOutput.PASS;
+		String traceback = "";
+		if(matcher_fail.find()) {
+			status = TestOutput.FAIL;
+		} else if(matcher_error.find()) {
+			status = TestOutput.ERROR;
+		}
+		Matcher matcher_traceback = TRACEBACK_PATTERN.matcher(s);
+		if(matcher_traceback.find()) {
+			traceback = matcher_traceback.group(1);
+		}
 		
+		return new TestOutput(status, traceback);
 	}
 
 	/*
@@ -117,6 +155,41 @@ public class PythonTestRunner extends Runner {
 			runPyTests(notifier);
 		} catch (IOException e) {
 			System.out.println(e.getMessage());
+		}
+	}
+	
+	private class TestOutput
+	{
+		public static final int PASS = 0;
+		public static final int FAIL = 1;
+		public static final int ERROR = 2;
+		private int status; // 0 - pass, 1 - fail, 2 - error
+		private String traceback;
+		
+		public TestOutput(int status, String traceback)
+		{
+			this.status = status;
+			this.traceback = (traceback == null ? "" : traceback);
+		}
+		
+		public boolean getFailed()
+		{
+			return this.status == FAIL;
+		}
+		
+		public boolean getError()
+		{
+			return this.status == ERROR;
+		}
+		
+		public boolean getPass()
+		{
+			return this.status == PASS;
+		}
+		
+		public String getTraceback()
+		{
+			return this.traceback;
 		}
 	}
 
