c0301937a23ccbced93a8206386bf20421e98aac
andreas muelder <muelder@itemis.de>
2018-07-06 11:26:07 +0200

    testing scaling on mac

--- plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/utils/FontScalingUtil.java
+++ plugins/org.yakindu.sct.ui.editor/src/org/yakindu/sct/ui/editor/utils/FontScalingUtil.java
@@ -12,24 +12,20 @@ import org.yakindu.sct.ui.editor.preferences.StatechartPreferenceConstants;
  */
 public class FontScalingUtil {
 
-	private static final int WINDOWS_DEFAULT_DPI = 96;
-	private static final String OPERATING_SYSTEM = System.getProperty("os.name").toLowerCase();
+	private static final int DEFAULT_DPI = 96;
 
 	public static FontData scaleFont(FontData fontData) {
 		return scaleFont(fontData, 0);
 	}
 
 	public static FontData scaleFont(FontData fontData, int style) {
-		if (OPERATING_SYSTEM.indexOf("win") == -1 || DiagramActivator.getDefault().getPreferenceStore()
+		if (DiagramActivator.getDefault().getPreferenceStore()
 				.getBoolean(StatechartPreferenceConstants.PREF_FONT_SCALING)) {
 			return fontData;
 		}
 		int DPI = Display.getCurrent().getDPI().y;
-		if (DPI != WINDOWS_DEFAULT_DPI) {
-			double factor = (double) WINDOWS_DEFAULT_DPI / DPI;
-			return new FontData(fontData.getName(), (int) (fontData.getHeight() * factor), fontData.getStyle() | style);
-		}
-		return fontData;
-
+		double factor = (double) DEFAULT_DPI / DPI;
+		return new FontData(fontData.getName(), (int) (fontData.getHeight() * factor), fontData.getStyle() | style);
 	}
+
 }
